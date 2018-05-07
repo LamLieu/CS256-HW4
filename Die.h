9858f9d1480fb0df79e37f58784df1ff145d2768
LamLieu <lieulam@hotmail.com>
2018-05-07 12:33:12 -0700

    Initial Commit

---
+++ Die.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "stdafx.h"
+
+class Die {
+private:
+	int sides; // Number of sides
+	int value; // The die's value
+	
+public:
+	Die(int = 6); // Constructor
+	void roll(); // Rolls the die
+	int getSides(); // Returns the number of sides
+	int getValue(); // Returns the die's value
+};
