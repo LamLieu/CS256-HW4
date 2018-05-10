1768d5222c2b25bb63989970c7a9678e6bb1da32
Lam Lieu <35049466+LamLieu@users.noreply.github.com>
2018-05-10 14:09:09 -0700

    Rename ParkingTicketSim.cpp to ParkingTicketSim/ParkingTicketSim.cpp

---
+++ ParkingTicketSim/ParkingTicketSim.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "PoliceOfficer.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	PoliceOfficer officer;
+	ParkingMeter meter;
+
+	bool timeExpired = officer.checkIfTimeExpired(meter);
+	if (timeExpired) {
+		officer.issueParkingTicket(meter, officer);
+	}
+	else { 
+		cout << "Officer didn't isuse ticket" << endl;
+	}
+
+    return 0;
+}
+
